f19ef2349b1e6ce95c11fc8397c78f96c0524fb4
Argha Chakrabarty <argha.chakrabarty@girnarsoft.com>
2025-02-10 23:27:13 +0530

    init pygame

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import pygame
+import sys
+
+
 def main():
-    print("Namaste Duniya")
+    pygame.init()
+
+    screen_width = 800
+    screen_height = 600
+
+    screen = pygame.display.set_mode((screen_width, screen_height))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill((255, 255, 255))
+
+        pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
 
 if __name__ == "__main__":
     main()
